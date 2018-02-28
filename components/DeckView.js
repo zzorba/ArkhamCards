178dbfe7eef9745454fe5c4d657b9e8928d17ad5
Daniel Salinas <daniel@scroll.com>
2018-02-28 10:05:23 -0500

    Working (unstyled) segmented display

--- components/DeckView.js
+++ components/DeckView.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { sum, uniqBy } from 'lodash';
 const {
   StyleSheet,
-  ListView,
+  SectionList,
   View,
   Text,
   ActivityIndicator
@@ -20,13 +21,13 @@ function filterBy(cardIds, cards, field, value) {
 function groupAssets(cardIds, cards) {
   const assets = filterBy(cardIds, cards, 'type_code', 'asset');
   return [
-    { type: 'Hand', cardIds: filterBy(assets, cards, 'slot', 'Hand') },
-    { type: 'Arcane', cardIds: filterBy(assets, cards, 'slot', 'Arcane') },
-    { type: 'Accessory', cardIds: filterBy(assets, cards, 'slot', 'Accessory') },
-    { type: 'Body', cardIds: filterBy(assets, cards, 'slot', 'Body') },
-    { type: 'Ally', cardIds: filterBy(assets, cards, 'slot', 'Ally') },
-    { type: 'Other', cardIds: filterBy(assets, cards, 'slot', undefined) },
-  ].filter(asset => asset.cardIds.size > 0);
+    { type: 'Hand', data: filterBy(assets, cards, 'slot', 'Hand') },
+    { type: 'Arcane', data: filterBy(assets, cards, 'slot', 'Arcane') },
+    { type: 'Accessory', data: filterBy(assets, cards, 'slot', 'Accessory') },
+    { type: 'Body', data: filterBy(assets, cards, 'slot', 'Body') },
+    { type: 'Ally', data: filterBy(assets, cards, 'slot', 'Ally') },
+    { type: 'Other', data: filterBy(assets, cards, 'slot', undefined) },
+  ].filter(asset => asset.data.length > 0);
 }
 
 function isSpecialCard(card) {
@@ -39,12 +40,12 @@ function splitCards(cardIds, cards) {
   const result = {};
 
   const groupedAssets = groupAssets(cardIds, cards);
-  if (groupedAssets.size > 0) {
+  if (groupedAssets.length > 0) {
     result.assets = groupedAssets;
   }
-  ['event', 'skill', 'treachery', 'enemy'].forEach(type_code => {
-    const typeCards = filterBy(cardIds, cards, 'type_code', type_code);
-    if (typeCards.size > 0) {
+  ['Event', 'Skill', 'Treachery', 'Enemy'].forEach(type_code => {
+    const typeCards = filterBy(cardIds, cards, 'type_code', type_code.toLowerCase());
+    if (typeCards.length > 0) {
       result[type_code] = typeCards;
     }
   })
@@ -65,19 +66,19 @@ function parseDeck(deck, cards) {
       quantity: deck.slots[id],
     };
   });
-  const investigator = cardIds.find(c => cards[c.id].type_code === 'investigator');
-  const specials = cardsIds.filter(c => isSpecialCard(cards[c.id]) && c.id !== investigator.id);
-  const normalCards = specials.filter(c => !isSpecialCard(cards[c.id]) && c.id !== investigator.id);
+  const investigator = deck.investigator_code;
+  const specialCards = cardIds.filter(c => isSpecialCard(cards[c.id]));
+  const normalCards = cardIds.filter(c => !isSpecialCard(cards[c.id]));
   return {
-    investigator: cards[investigator.id],
+    investigator: cards[deck.investigator_code],
     meta: {
-      normalCardCount: normalCards.map(c => c.quantity).sum(),
-      totalCardCount: cardIds.map(c => c.quantity).sum(),
-      experience: cardIds.map(c => computeXp(cards[c.id]) * c.quantity).sum(),
-      packs: cardIds.map(c => cards[c.id].pack_code).distinct().size,
+      normalCardCount: sum(normalCards.map(c => c.quantity)),
+      totalCardCount: sum(cardIds.map(c => c.quantity)),
+      experience: sum(cardIds.map(c => computeXp(cards[c.id]) * c.quantity)),
+      packs: uniqBy(cardIds, c => cards[c.id].pack_code).length,
     },
     normal: splitCards(normalCards, cards, deck.slots),
-    special: splitCards(special, cards, deck.slots),
+    special: splitCards(specialCards, cards, deck.slots),
   };
 }
 
@@ -91,29 +92,72 @@ class DeckView extends React.Component {
   constructor(props) {
     super(props);
 
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
-    });
-    this.state = {
-      ds,
-      parsedDeck: parseDeck(props.deck, props.cards),
-    };
-
     this._renderCard = this.renderCard.bind(this);
+    this._renderCardHeader = this.renderCardHeader.bind(this);
+    this._keyForCard = this.keyForCard.bind(this);
   }
 
   componentDidMount() {
     this.props.getDeck(this.props.id);
   }
 
-  renderCard(rowData, sectionID, rowID) {
-    const card = this.props.cards[rowData.cardId];
+  keyForCard(item) {
+    return item.id;
+  }
+
+  renderCardHeader({ section }) {
+    console.log(section);
     return (
       <View>
-        <Text>{`${rowData.quantity}x ${card.name}`}</Text>
+        <Text>{section.title || section.subTitle} </Text>
+      </View>
+    );
+  }
+
+  renderCard({ item }) {
+    const card = this.props.cards[item.id];
+    if (!card) {
+      console.log(`Could not find ${item.id}`);
+      return null;
+    }
+    return (
+      <View key={item.id}>
+        <Text style={styles.defaultText}>
+          {`${item.quantity}x `}
+          <Text style={styles[card.faction_code]}>
+            {card.name}
+          </Text>
+        </Text>
       </View>
     )
+  }
+
+  deckToSections(halfDeck) {
+    const result = [];
+    if (halfDeck.assets) {
+      const assetCount = sum(halfDeck.assets.map(subAssets =>
+         sum(subAssets.data.map(c => c.quantity))))
+      result.push({
+        title: `Assets (${assetCount})`,
+        data: [],
+      });
+      halfDeck.assets.forEach(subAssets => {
+        result.push({
+          subTitle: subAssets.type,
+          data: subAssets.data,
+        });
+      });
+    }
+    ['Event', 'Skill', 'Treachery', 'Enemy'].forEach(type => {
+      if (halfDeck[type]) {
+        const count = sum(halfDeck[type].map(c => c.quantity));
+        result.push({
+          title: `${type} (${count})`,
+          data: halfDeck[type],
+        });
+      }
+    });
+    return result;
   }
 
   render() {
@@ -129,19 +173,21 @@ class DeckView extends React.Component {
       );
     }
 
-    const deckList = Object.keys(deck.slots).map(cardId => {
-      return {
-        cardId,
-        quantity: deck.slots[cardId],
-      };
-    })
+    const parsedDeck = parseDeck(deck, cards);
+
+    const sections =
+      this.deckToSections(parsedDeck.normal)
+        .concat(this.deckToSections(parsedDeck.special));
+
     return (
       <View>
         <Text>{ deck.name }</Text>
-        <ListView
-          enableEmptySections={true}
-          dataSource={this.state.ds.cloneWithRows(deckList)}
-          renderRow={this._renderCard} />
+        <SectionList
+          renderItem={this._renderCard}
+          keyExtractor={this._keyForCard}
+          renderSectionHeader={this._renderCardHeader}
+          sections={sections}
+        />
       </View>
     );
   }
@@ -171,3 +217,27 @@ function mapDispatchToProps(dispatch) {
 
 //Connect everything
 export default connect(mapStateToProps, mapDispatchToProps)(DeckView);
+
+var styles = StyleSheet.create({
+  defaultText: {
+    color: '#000000',
+  },
+  seeker: {
+    color: '#ec8426',
+  },
+  guardian: {
+    color: '#2b80c5',
+  },
+  mystic: {
+    color: '#4331b9',
+  },
+  rogue: {
+    color: '#107116',
+  },
+  survivor: {
+    color: '#cc3038',
+  },
+  neutral: {
+    color: '#606060',
+  },
+});
