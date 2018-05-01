f52db2a3dba4405cdb2621012dce5b8c4bf6c8cd
Daniel Salinas <daniel@scroll.com>
2018-04-30 23:41:11 -0400

    Fix styling of text box on android

--- components/core/TextBox.js
+++ components/core/TextBox.js
@@ -9,7 +9,12 @@ import {
 export default function TextBox({ value, ...otherProps }) {
   return (
     <View style={styles.textBox}>
-      <TextInput style={styles.input} value={value} {...otherProps} />
+      <TextInput
+        underlineColorAndroid="transparent"
+        style={styles.input}
+        value={value}
+        {...otherProps}
+      />
     </View>
   );
 }
@@ -20,17 +25,26 @@ TextBox.propTypes = {
 
 const styles = StyleSheet.create({
   textBox: {
-    height: 24,
+    height: 26,
     width: '100%',
     backgroundColor: 'white',
     borderWidth: 2,
     borderRadius: 4,
     borderColor: '#444',
+    paddingTop: 2,
+    paddingBottom: 2,
     paddingLeft: 4,
     paddingRight: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   input: {
+    fontFamily: 'System',
     fontSize: 16,
+    color: '#222',
+    lineHeight: 22,
     width: '100%',
+    height: 22,
+    padding: 0,
   },
 });
