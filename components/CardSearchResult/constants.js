baef4a30cae02e4ac05f660999edcef4b2f44918
Daniel Salinas <daniel@scroll.com>
2019-03-03 20:55:22 -0500

    Adjust breakpoint for toggle count button

--- components/CardSearchResult/constants.js
+++ components/CardSearchResult/constants.js
@@ -8,7 +8,7 @@ export const ROW_HEIGHT = (isBig ? 72 : 48) * DeviceInfo.getFontScale();
 export const ICON_SIZE = (isBig ? 44 : 28) * HALF_FONT_SCALE;
 export const BUTTON_PADDING = 12;
 export const BUTTON_WIDTH = 18 * DeviceInfo.getFontScale() + 22;
-export const TOGGLE_BUTTON_MODE = TINY_PHONE || DeviceInfo.getFontScale() > 1.5;
+export const TOGGLE_BUTTON_MODE = TINY_PHONE || DeviceInfo.getFontScale() > 1.3;
 
 export default {
   ROW_HEIGHT,
