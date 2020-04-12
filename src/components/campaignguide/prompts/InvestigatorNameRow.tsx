03b57f3fe2fd7de3571cd3b6546dc266cbfa3c46
Daniel Salinas <daniel@scroll.com>
2020-04-11 20:45:35 -0400

    Soften campaign log entries in supplies

--- src/components/campaignguide/prompts/InvestigatorNameRow.tsx
+++ src/components/campaignguide/prompts/InvestigatorNameRow.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { FACTION_COLORS } from 'constants';
+import { FACTION_COLORS, FACTION_LIGHT_GRADIENTS } from 'constants';
 import Card from 'data/Card';
 import typography from 'styles/typography';
 import space from 'styles/space';
@@ -9,19 +9,31 @@ import space from 'styles/space';
 interface Props {
   investigator: Card;
   detail?: string;
+  dark?: boolean;
 }
 
-export default function InvestigatorNameRow({ investigator, detail }: Props) {
-  const backgroundColor = FACTION_COLORS[investigator.factionCode()];
+export default function InvestigatorNameRow({
+  investigator,
+  detail,
+  dark,
+}: Props) {
+  const backgroundColor = dark ?
+    FACTION_COLORS[investigator.factionCode()] :
+    FACTION_LIGHT_GRADIENTS[investigator.factionCode()][0];
   return (
     <View style={[
       styles.investigatorRow,
       space.paddingS,
       space.paddingLeftM,
       { backgroundColor },
+      dark ? { borderColor: '#FFF' } : { borderColor: '#888' },
     ]}>
       <View>
-        <Text style={[typography.text, styles.investigatorText]}>
+        <Text style={[
+          typography.mediumGameFont,
+          styles.investigatorText,
+          dark ? { color: '#FFF' } : { color: '#000' },
+        ]}>
           { investigator.name }
         </Text>
       </View>
@@ -38,11 +50,12 @@ export default function InvestigatorNameRow({ investigator, detail }: Props) {
 
 const styles = StyleSheet.create({
   investigatorText: {
-    color: '#FFF',
     fontWeight: '600',
   },
   investigatorRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
