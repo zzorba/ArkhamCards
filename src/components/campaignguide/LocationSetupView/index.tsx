41761b5f301a35802c979f6a845a875d251ff819
Daniel Salinas <daniel@scroll.com>
2020-05-25 18:19:46 -0400

    Fix LocationSetupView on small phones

--- src/components/campaignguide/LocationSetupView/index.tsx
+++ src/components/campaignguide/LocationSetupView/index.tsx
@@ -126,6 +126,7 @@ class LocationSetupView extends React.Component<Props> {
   render() {
     const {
       step,
+      width,
     } = this.props;
     if (!step) {
       return null;
@@ -143,7 +144,7 @@ class LocationSetupView extends React.Component<Props> {
     return (
       <PinchZoomView style={{ width: 500 }}>
         { !!note && (
-          <View style={{ width: '100%' }}>
+          <View style={{ width: width }}>
             <SetupStepWrapper bulletType="none">
               <CampaignGuideTextComponent text={note} />
             </SetupStepWrapper>
