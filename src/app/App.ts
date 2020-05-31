895d0d2bdb9363db69b2d368af8fa86515a07772
Daniel Salinas <daniel@scroll.com>
2020-05-31 10:28:26 -0400

    Eliminate some warnings in 3rd party deps

--- src/app/App.ts
+++ src/app/App.ts
@@ -49,9 +49,6 @@ export default class App {
       'Warning: Failed prop type: DialogSwitch: prop type `labelStyle` is invalid;',
       'Warning: `flexWrap: `wrap`` is not supported with the `VirtualizedList` components.' +
       'Consider using `numColumns` with `FlatList` instead.',
-      'Warning: Failed prop type: Invalid prop `rules.emMarkdown.order` of type `number` supplied to `MarkdownView`, expected `function`.',
-      'Warning: Failed prop type: Invalid prop `rules.uTag.order` of type `number` supplied to `MarkdownView`, expected `function`.',
-      'Warning: Failed prop type: Invalid prop `rules.bTag.order` of type `number` supplied to `MarkdownView`, expected `function`.',
     ]);
 
     const browseCards = {
