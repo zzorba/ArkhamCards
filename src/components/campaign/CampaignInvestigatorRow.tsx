99732040df2952933937a492ed17b7a51ed25d83
Daniel Salinas <daniel@occamzrazor.com>
2020-10-19 22:29:15 -0400

    Hookify a few more low-hanging items for Player cards

--- src/components/campaign/CampaignInvestigatorRow.tsx
+++ src/components/campaign/CampaignInvestigatorRow.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { filter, find, map } from 'lodash';
 import {
   StyleSheet,
   View,
 } from 'react-native';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { Campaign, Deck } from '@actions/types';
 import { BODY_OF_A_YITHIAN } from '@app_constants';
 import InvestigatorImage from '@components/core/InvestigatorImage';
-import withPlayerCards, { PlayerCardProps } from '@components/core/withPlayerCards';
 import { getDecks, getLatestCampaignDeckIds, AppState } from '@reducers';
 import { s } from '@styles/space';
+import useInvestigatorCards from '@components/core/useInvestigatorCards';
 
-interface OwnProps {
+interface Props {
   campaigns: Campaign[];
 }
 
@@ -22,122 +22,69 @@ interface CampaignDecks {
   decks: Deck[];
 }
 
-interface ReduxProps {
-  campaignDecks: CampaignDecks[];
-}
-
-type Props = OwnProps & ReduxProps & PlayerCardProps;
-
-class CampaignInvestigatorRow extends React.Component<Props> {
-  _renderInvestigator = (
-    code: string,
-    campaign: Campaign,
-    deck?: Deck
-  ) => {
-    const {
-      investigators,
-    } = this.props;
-    const { investigatorData } = campaign;
-    const card = investigators[code];
-    if (card) {
-      const killedOrInsane = card.eliminated(investigatorData[code]);
-      const yithian = !!find(investigatorData.storyAssets || [], asset => asset === BODY_OF_A_YITHIAN) ||
-        (deck && (deck.slots[BODY_OF_A_YITHIAN] || 0) > 0);
-      return (
-        <View key={card.code} style={styles.investigator}>
-          <InvestigatorImage
-            card={card}
-            killedOrInsane={killedOrInsane}
-            yithian={yithian}
-            border
-            small
-          />
-        </View>
-      );
-    }
-    return null;
-  };
-
-  _renderDeck = (deck: Deck, campaign: Campaign) => {
-    if (deck && deck.investigator_code) {
-      return this._renderInvestigator(deck.investigator_code, campaign, deck);
-    }
-    return null;
-  };
-
-  render() {
-    const {
-      campaignDecks,
-    } = this.props;
-    return map(campaignDecks, ({ campaign, decks }) => {
-      const deckInvestigators = new Set(map(decks, deck => deck.investigator_code));
-      return (
-        <View key={campaign.id} style={styles.row}>
-          { map(decks, deck => this._renderDeck(deck, campaign)) }
-          { map(
-            filter(
-              campaign.nonDeckInvestigators || [],
-              code => !deckInvestigators.has(code)
-            ),
-            code => this._renderInvestigator(code, campaign)
-          ) }
-        </View>
-      );
-    });
-  }
-}
-
-function mapStateToProps(state: AppState, props: OwnProps): ReduxProps {
-  return {
-    campaignDecks: map(props.campaigns, campaign => {
+export default function CampaignInvestigatorRow({ campaigns }: Props) {
+  const campaignDecksSelector = useCallback((state: AppState): CampaignDecks[] => {
+    return map(campaigns, campaign => {
       const latestDeckIds = getLatestCampaignDeckIds(state, campaign);
       return {
         campaign,
         decks: getDecks(state, latestDeckIds),
       };
-    }),
-  };
-}
-
-class PlaceholderCampaignInvestigatorRow extends React.Component<OwnProps & ReduxProps> {
-  renderPlaceholder(id: number | string) {
+    });
+  }, [campaigns]);
+  const campaignDecks = useSelector(campaignDecksSelector);
+  const investigators = useInvestigatorCards();
+  const renderInvestigator = useCallback((
+    code: string,
+    campaign: Campaign,
+    deck?: Deck
+  ) => {
+    const { investigatorData } = campaign;
+    const card = investigators?.[code];
+    const killedOrInsane = card && card.eliminated(investigatorData[code]);
+    const yithian = !!find(investigatorData.storyAssets || [], asset => asset === BODY_OF_A_YITHIAN) ||
+      (deck && (deck.slots[BODY_OF_A_YITHIAN] || 0) > 0);
     return (
-      <View key={id} style={styles.investigator}>
+      <View key={code} style={styles.investigator}>
         <InvestigatorImage
+          card={card}
+          killedOrInsane={killedOrInsane}
+          yithian={yithian}
           border
           small
         />
       </View>
     );
-  }
-  render() {
-    const {
-      campaignDecks,
-    } = this.props;
-    return map(campaignDecks, ({ campaign, decks }) => {
-      const deckInvestigators = new Set(map(decks, deck => deck.investigator_code));
-      return (
-        <View key={campaign.id} style={styles.row}>
-          { map(decks, deck => this.renderPlaceholder(deck.id)) }
-          { map(
-            filter(
-              campaign.nonDeckInvestigators || [],
-              code => !deckInvestigators.has(code)
-            ),
-            code => this.renderPlaceholder(code)
-          ) }
-        </View>
-      );
-    });
-  }
-}
+  }, [investigators]);
 
-export default connect(mapStateToProps)(
-  withPlayerCards(
-    CampaignInvestigatorRow,
-    PlaceholderCampaignInvestigatorRow
-  )
-);
+  const renderDeck = useCallback((deck: Deck, campaign: Campaign) => {
+    if (deck && deck.investigator_code) {
+      return renderInvestigator(deck.investigator_code, campaign, deck);
+    }
+    return null;
+  }, [renderInvestigator]);
+
+
+  return (
+    <>
+      { map(campaignDecks, ({ campaign, decks }) => {
+        const deckInvestigators = new Set(map(decks, deck => deck.investigator_code));
+        return (
+          <View key={campaign.id} style={styles.row}>
+            { map(decks, deck => renderDeck(deck, campaign)) }
+            { map(
+              filter(
+                campaign.nonDeckInvestigators || [],
+                code => !deckInvestigators.has(code)
+              ),
+              code => renderInvestigator(code, campaign)
+            ) }
+          </View>
+        );
+      }) }
+    </>
+  );
+}
 
 const styles = StyleSheet.create({
   row: {
--- src/components/campaign/OddsCalculatorView.tsx
+++ src/components/campaign/OddsCalculatorView.tsx
@@ -1,98 +1,50 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { flatMap, forEach } from 'lodash';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import OddsCalculatorComponent from './OddsCalculatorComponent';
 import { NavigationProps } from '@components/nav/types';
-import withPlayerCards, { PlayerCardProps } from '@components/core/withPlayerCards';
 import CardQueryWrapper from '@components/card/CardQueryWrapper';
-import withDimensions, { DimensionsProps } from '@components/core/withDimensions';
 import { campaignScenarios, Scenario } from '@components/campaign/constants';
-import { Campaign } from '@actions/types';
-import { ChaosBag } from '@app_constants';
-import Card from '@data/Card';
 import { SCENARIO_CARDS_QUERY } from '@data/query';
 import { AppState, getCampaign } from '@reducers';
+import useInvestigatorCards from '@components/core/usePlayerCards';
 
 export interface OddsCalculatorProps {
   campaignId: number;
   investigatorIds: string[];
 }
 
-interface ReduxProps {
-  campaign?: Campaign;
-  chaosBag?: ChaosBag;
-  cycleScenarios?: Scenario[];
-  scenarioByCode?: { [code: string]: Scenario };
-}
+const EMPTY_CHAOS_BAG = {};
+export default function OddsCalculatorView({ campaignId, investigatorIds, componentId}: OddsCalculatorProps & NavigationProps) {
+  const campaign = useSelector((state: AppState) => getCampaign(state, campaignId));
+  const chaosBag = campaign?.chaosBag || EMPTY_CHAOS_BAG;
+  const cycleScenarios = useMemo(() => campaign ? campaignScenarios(campaign.cycleCode) : [], [campaign]);
+  const scenarioByCode = useMemo(() => {
+    const result: { [code: string]: Scenario } = {};
+    forEach(cycleScenarios, scenario => {
+      result[scenario.code] = scenario;
+    });
+    return result;
+  }, [cycleScenarios]);
+  const investigators = useInvestigatorCards();
+  const allInvestigators = useMemo(() => flatMap(investigatorIds, code => investigators?.[code] || []), [investigatorIds, investigators]);
 
-type Props = NavigationProps & OddsCalculatorProps & ReduxProps & PlayerCardProps & DimensionsProps;
-
-class OddsCalculatorView extends React.Component<Props> {
-  allInvestigators(): Card[] {
-    const {
-      investigators,
-      investigatorIds,
-    } = this.props;
-    return flatMap(investigatorIds, code => investigators[code] || []);
-  }
-
-  render() {
-    const {
-      campaign,
-      chaosBag,
-      cycleScenarios,
-      scenarioByCode,
-    } = this.props;
-    if (!campaign) {
-      return null;
-    }
-
-    const allInvestigators = this.allInvestigators();
-    return (
-      <CardQueryWrapper name="odds" query={SCENARIO_CARDS_QUERY}>
-        { scenarioCards => (
-          <OddsCalculatorComponent
-            campaign={campaign}
-            chaosBag={chaosBag || {}}
-            cycleScenarios={cycleScenarios}
-            scenarioByCode={scenarioByCode}
-            allInvestigators={allInvestigators}
-            scenarioCards={scenarioCards}
-          />
-        ) }
-      </CardQueryWrapper>
-    );
-  }
-}
-
-function mapStateToProps(
-  state: AppState,
-  props: OddsCalculatorProps
-): ReduxProps {
-  const campaign = getCampaign(state, props.campaignId);
   if (!campaign) {
-    return {
-      chaosBag: {},
-      cycleScenarios: [],
-      scenarioByCode: {},
-    };
+    return null;
   }
-  const cycleScenarios = campaignScenarios(campaign.cycleCode);
-  const scenarioByCode: { [code: string]: Scenario } = {};
-  forEach(cycleScenarios, scenario => {
-    scenarioByCode[scenario.code] = scenario;
-  });
-  return {
-    campaign,
-    chaosBag: campaign.chaosBag || {},
-    cycleScenarios,
-    scenarioByCode,
-  };
+  return (
+    <CardQueryWrapper name="odds" query={SCENARIO_CARDS_QUERY}>
+      { scenarioCards => (
+        <OddsCalculatorComponent
+          campaign={campaign}
+          chaosBag={chaosBag || {}}
+          cycleScenarios={cycleScenarios}
+          scenarioByCode={scenarioByCode}
+          allInvestigators={allInvestigators}
+          scenarioCards={scenarioCards}
+        />
+      ) }
+    </CardQueryWrapper>
+  );
 }
-
-export default connect(mapStateToProps)(
-  withPlayerCards<NavigationProps & OddsCalculatorProps & ReduxProps>(
-    withDimensions(OddsCalculatorView)
-  )
-);
