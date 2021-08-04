29cd834eb3c49cea84559b182de2b193b342061f
Daniel Salinas <daniel@occamzrazor.com>
2021-08-04 14:43:39 -0400

    Adjust tokens with full text

---
+++ src/components/campaign/OddsCalculatorComponent/TokenTextLine.tsx
@@ -0,0 +1,52 @@
+import React, { useCallback, useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import CardTextComponent from '@components/card/CardTextComponent';
+import space from '@styles/space';
+import StyleContext from '@styles/StyleContext';
+import { ChaosTokenType } from '@app_constants';
+import ChaosToken from '../ChaosToken';
+
+export interface Props {
+  symbol: ChaosTokenType;
+  text: string;
+}
+
+export default function TokenTextLine({
+  text,
+  symbol,
+}: Props) {
+  const { colors } = useContext(StyleContext);
+  return (
+    <View style={[styles.input, { backgroundColor: colors.L30 }, space.marginSideS, space.marginTopS]}>
+      <View style={[styles.row, space.paddingS]}>
+        <ChaosToken
+          iconKey={symbol}
+          size="extraTiny"
+        />
+      </View>
+      <View style={styles.text}>
+        <CardTextComponent text={text} />
+      </View>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  text: {
+    flex: 1,
+    marginRight: 15,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+});
--- src/components/campaign/OddsCalculatorComponent/VariableTokenInput.tsx
+++ src/components/campaign/OddsCalculatorComponent/VariableTokenInput.tsx
@@ -7,11 +7,13 @@ import space, { s } from '@styles/space';
 import StyleContext from '@styles/StyleContext';
 import { ChaosTokenType } from '@app_constants';
 import ChaosToken from '../ChaosToken';
+import TokenTextLine from './TokenTextLine';
 
 export interface Props {
   symbol: ChaosTokenType;
   value: number;
-  text: string;
+  text?: string;
+  prompt: string;
   min: number;
   max?: number;
   increment: (symbol: string) => void;
@@ -20,6 +22,7 @@ export interface Props {
 
 export default function VariableTokenInput({
   text,
+  prompt,
   value,
   symbol,
   min,
@@ -37,30 +40,37 @@ export default function VariableTokenInput({
   }, [decrement, symbol]);
 
   return (
-    <View style={[styles.input, { backgroundColor: colors.L20 }, space.marginSideS, space.marginTopS]}>
-      <View style={[styles.row, space.paddingS]}>
-        <ChaosToken
-          iconKey={symbol}
-          size="extraTiny"
-        />
+    <>
+      { !!text && <TokenTextLine symbol={symbol} text={text} /> }
+      <View style={[styles.input, { backgroundColor: colors.L20, minHeight: 36 + s * 2 }, space.marginSideS, space.marginTopS]}>
+        <View style={[styles.row, space.paddingS]}>
+          { !text ? (
+            <ChaosToken
+              iconKey={symbol}
+              size="extraTiny"
+            />
+          ) : (
+            <View style={{ width: 36 }} />
+          ) }
+        </View>
+        <View style={styles.text}>
+          <CardTextComponent text={prompt} />
+        </View>
+        <View style={[styles.row, { borderColor: colors.L10, borderLeftWidth: StyleSheet.hairlineWidth }, space.paddingSideS]}>
+          <PlusMinusButtons
+            count={value}
+            size={36}
+            dialogStyle
+            countRender={<Text style={[typography.text, styles.counterText, typography.center]}>{ value }</Text>}
+            onIncrement={inc}
+            onDecrement={dec}
+            min={min}
+            max={max}
+            color="dark"
+          />
+        </View>
       </View>
-      <View style={styles.text}>
-        <CardTextComponent text={text} />
-      </View>
-      <View style={[styles.row, { borderColor: colors.L10, borderLeftWidth: StyleSheet.hairlineWidth }, space.paddingLeftS]}>
-        <PlusMinusButtons
-          count={value}
-          size={36}
-          dialogStyle
-          countRender={<Text style={[typography.text, styles.counterText, typography.center]}>{ value }</Text>}
-          onIncrement={inc}
-          onDecrement={dec}
-          min={min}
-          max={max}
-          color="dark"
-        />
-      </View>
-    </View>
+    </>
   );
 }
 
