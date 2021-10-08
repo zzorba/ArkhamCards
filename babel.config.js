0372d39ad8db53e5745bbe450d54ee8d29b36346
Daniel Salinas <daniel@occamzrazor.com>
2021-10-07 23:18:22 -0500

    Fix babel screwups

--- babel.config.js
+++ babel.config.js
@@ -28,9 +28,6 @@ module.exports = {
         },
       },
     ],
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
-    ["@babel/plugin-proposal-private-methods", { "loose": true }],
   ]
 };
 
