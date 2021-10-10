25138cca350f5527b3a73285bb3bc94128dcae50
Daniel Salinas <daniel@occamzrazor.com>
2021-10-10 11:18:37 -0500

    Clean up publicApi slightly, start parsing the sqlite version

--- src/lib/publicApi.ts
+++ src/lib/publicApi.ts
@@ -135,7 +135,7 @@ export const syncTaboos = async function(
 };
 
 const SQLITE_NUM_VARIABLES = 999;
-const ANDROID_NUM_CARDS_PER_INSERT = Math.floor(SQLITE_NUM_VARIABLES / CARD_NUM_COLUMNS);
+const ANDROID_NUM_CARDS_PER_INSERT = Math.max(Math.floor(SQLITE_NUM_VARIABLES / CARD_NUM_COLUMNS) - 1, 1);
 const IOS_LIMIT = Platform.OS === 'ios' && parseInt(`${Platform.Version}`, 10) >= 14 ? Math.floor(32766 / CARD_NUM_COLUMNS) : 50;
 const MAX_CARDS_PER_INSERT = Platform.OS === 'ios' ? IOS_LIMIT : ANDROID_NUM_CARDS_PER_INSERT;
 async function insertChunk<T>(things: T[], insert: (things: T[]) => Promise<any>, maxInsert?: number) {
@@ -235,241 +235,236 @@ export const syncCards = async function(
     }
     updateProgress(0.03);
 
-    try {
-      const customCardsPromise = anonClient.query<GetCustomCardsQuery, GetCustomCardsQueryVariables>({
-        query: GetCustomCardsDocument,
-        variables: {
-          locale: lang || 'en',
-        },
-        fetchPolicy: 'network-only',
-      });
-      const response = await fetch(uri, {
-        method: 'GET',
-        headers,
-      });
-      if (response.status === 304 && cache) {
-        updateProgress(0.5);
-        try {
-          const customCardsResponse = await customCardsPromise;
-          const customCards = map(customCardsResponse.data.card, customCard => Card.fromGraphQl(customCard, lang || 'en'));
-          const queryRunner = await db.startTransaction();
-          try {
-            await insertChunk(customCards, async(c: Card[]) => {
-              await queryRunner.manager.delete(Card, map(c, c => c.id));
-              await queryRunner.manager.insert(Card, c);
-            });
-            updateProgress(0.7);
-          } finally {
-            await queryRunner.commitTransaction();
-            await queryRunner.release();
-          }
-        } catch (e) {
-          console.log(e);
-        }
-        updateProgress(0.9);
-        return cache;
-      }
-      VERBOSE && console.log('Got results from ArkhamDB');
-
-      const lastModified = response.headers.get('Last-Modified') || undefined;
-      updateProgress(0.1);
-
-      const json = await response.json();
-      updateProgress(0.2);
-      VERBOSE && console.log('Parsed ArkhamDB json');
-
-      const encounterSets = await db.encounterSets();
-      const cards = await db.cards();
-      const tabooSets = await db.tabooSets();
-      const rules = await db.rules();
-
-      // Delete the tables.
-      await cards.createQueryBuilder().delete().execute();
-      await encounterSets.createQueryBuilder().delete().execute();
-      await tabooSets.createQueryBuilder().delete().execute();
-      await rules.createQueryBuilder().delete().execute();
-      await db.clearCache();
-      VERBOSE && console.log('Cleared old database');
-      updateProgress(0.22);
-
-      await syncRules(db, lang);
-      updateProgress(0.25);
-
-      // console.log(`${await cards.count() } cards after delete`);
-      const genericInvestigator = Card.fromJson({
-        pack_code: 'custom',
-        pack_name: t`Custom`,
-        type_code: 'investigator',
-        type_name: t`Investigator`,
-        faction_code: 'neutral',
-        faction_name: t`Neutral`,
-        position: 1,
-        code: 'custom_001',
-        name: 'Johnny Anybody',
-        real_name: 'Johnny Anybody',
-        subname: t`The Chameleon`,
-        text: t`This is a custom investigator to allow for building of arbitrary decks.\n[elder_sign]: +X where X is whatever you want it to be.`,
-        quantity: 1,
-        skill_willpower: 3,
-        skill_intellect: 3,
-        skill_combat: 3,
-        skill_agility: 3,
-        health: 7,
-        health_per_investigator: false,
-        sanity: 7,
-        deck_limit: 1,
-        deck_requirements: {
-          size: 30,
-          card: {},
-          random: [{ target: 'subtype', value: 'basicweakness' }],
-        },
-        deck_options: [{
-          faction: ['guardian','seeker','rogue','mystic','survivor','neutral'],
-          level: { min: 0, max: 5 },
-        }],
-        is_unique: true,
-        double_sided: true,
-        back_text: '<b>Deck Size</b>: 30.\n<b>Deckbuilding Options</b>: Guardian cards ([guardian]) level 0-5, Seeker cards ([seeker]) level 0-5, Rogue cards ([rogue]) level 0-5, Mystic cards ([mystic]) level 0-5, Survivor cards ([survivor]) level 0-5, Neutral cards level 0-5.\n<b>Deckbuilding Requirements</b> (do not count toward deck size): 1 random basic weakness.',
-      }, packsByCode, cycleNames, lang || 'en');
-      genericInvestigator.browse_visible = 4;
-      const cardsToInsert: Card[] = [genericInvestigator];
-      const dupes: {
-        [code: string]: string[] | undefined;
-      } = {};
-      forEach(json, cardJson => {
-        try {
-          const card = Card.fromJson(cardJson, packsByCode, cycleNames, lang || 'en');
-          if (card) {
-            /*
-            // Code to spit out investigator deck_options localization strings.
-            if (card.type_code === 'investigator' && card.deck_options) {
-              forEach(card.deck_options, option => {
-                if (option.error) {
-                  console.log(`'${option.error}': t\`${option.error}\`,`);
-                }
-              });
-            }
-            */
-            if (card.duplicate_of_code) {
-              if (!dupes[card.duplicate_of_code]) {
-                dupes[card.duplicate_of_code] = [];
-              }
-              dupes[card.duplicate_of_code]?.push(card.pack_code);
-            }
-            cardsToInsert.push(card);
-          }
-        } catch (e) {
-          Alert.alert(`${e}`);
-          console.log(e);
-          console.log(cardJson);
-        }
-      });
-      updateProgress(0.35);
-      const linkedSet = new Set(flatMap(cardsToInsert, (c: Card) => c.linked_card ? [c.code, c.linked_card] : []));
-      const dedupedCards = filter(cardsToInsert, (c: Card) => !!c.linked_card || !linkedSet.has(c.code));
-      const flatCards = flatMap(dedupedCards, (c: Card) => {
-        return c.linked_card ? [c, c.linked_card] : [c];
-      });
-      const encounter_card_counts: {
-        [encounter_code: string]: number | undefined;
-      } = {};
-
-      // Clean up all the bonded stuff.
-      const bondedNames: string[] = [];
-      const playerCards: Card[] = [];
-      forEach(flatCards, card => {
-        if (dupes[card.code]) {
-          card.reprint_pack_codes = dupes[card.code];
-        }
-        if (!card.hidden && card.encounter_code) {
-          encounter_card_counts[card.encounter_code] = (encounter_card_counts[card.encounter_code] || 0) + (card.quantity || 1);
-        }
-        if (card.bonded_name) {
-          bondedNames.push(card.bonded_name);
-        }
-        if ((card.deck_limit && card.deck_limit > 0) && !card.spoiler && !(card.xp === undefined || card.xp === null)) {
-          playerCards.push(card);
-        }
-      });
-
-      // Handle all upgrade stuff
-      const cardsByName = values(groupBy(playerCards, card => card.real_name.toLowerCase()));
-      forEach(cardsByName, cardsGroup => {
-        if (cardsGroup.length > 1) {
-          const maxXpCard = head(sortBy(cardsGroup, card => -(card.xp || 0)));
-          if (maxXpCard) {
-            forEach(cardsGroup, card => {
-              const xp = card.xp || 0;
-              card.has_upgrades = xp < (maxXpCard.xp || 0);
-            });
-          }
-        }
-      });
-
-      // Handle all bonded card stuff, and encountercode sizes.
-      const bondedSet = new Set(bondedNames);
-      forEach(flatCards, card => {
-        if (card.encounter_code) {
-          card.encounter_size = encounter_card_counts[card.encounter_code] || 0;
-          encounter_card_counts;
-        }
-        if (bondedSet.has(card.real_name)) {
-          card.bonded_from = true;
-        }
-      });
-
-      // Deal with duplicate ids?
-      forEach(groupBy(flatCards, card => card.id), dupes => {
-        if (dupes.length > 1) {
-          forEach(dupes, (dupe, idx) => {
-            dupe.id = `${dupe.id}_${idx}`;
-          });
-        }
-      });
-      let customCards: Card[] = [];
+    const customCardsPromise = anonClient.query<GetCustomCardsQuery, GetCustomCardsQueryVariables>({
+      query: GetCustomCardsDocument,
+      variables: {
+        locale: lang || 'en',
+      },
+      fetchPolicy: 'network-only',
+    });
+    const response = await fetch(uri, {
+      method: 'GET',
+      headers,
+    });
+    if (response.status === 304 && cache) {
+      updateProgress(0.5);
       try {
         const customCardsResponse = await customCardsPromise;
-        customCards = map(customCardsResponse.data.card, customCard => Card.fromGraphQl(customCard, lang || 'en'));
+        const customCards = map(customCardsResponse.data.card, customCard => Card.fromGraphQl(customCard, lang || 'en'));
+        const queryRunner = await db.startTransaction();
+        try {
+          await insertChunk(customCards, async(c: Card[]) => {
+            await queryRunner.manager.delete(Card, map(c, c => c.id));
+            await queryRunner.manager.insert(Card, c);
+          });
+          updateProgress(0.7);
+        } finally {
+          await queryRunner.commitTransaction();
+          await queryRunner.release();
+        }
       } catch (e) {
         console.log(e);
       }
-      const [linkedCards, normalCards] = partition(dedupedCards, card => !!card.linked_card);
-      const queryRunner = await db.startTransaction();
-      try {
-        VERBOSE && console.log('Parsed all cards');
-        const totalCards = (linkedCards.length + normalCards.length + customCards.length + sumBy(linkedCards, c => c.linked_card ? 1 : 0)) || 3000;
-        let processedCards = 0;
-        async function insertCards(c: Card[]) {
-          await queryRunner.manager.insert(Card, c);
-          processedCards += c.length;
-          updateProgress(0.35 + (processedCards / (1.0 * totalCards) * 0.50));
-        }
-        await insertChunk(flatMap(linkedCards, c => c.linked_card ? [c.linked_card] : []), insertCards);
-        // console.log('Inserted back-link cards');
-        await insertChunk(linkedCards, insertCards);
-        VERBOSE && console.log('Inserted front link cards');
-        await insertChunk(normalCards, insertCards);
-        if (customCards.length) {
-          await insertChunk(customCards, insertCards);
-        }
-      } finally {
-        await queryRunner.commitTransaction();
-        await queryRunner.release();
-      }
+      updateProgress(0.9);
+      return cache;
+    }
+    VERBOSE && console.log('Got results from ArkhamDB');
 
-      VERBOSE && console.log('Inserted normal cards');
-      updateProgress(0.90);
-      const cardCount = await cards.createQueryBuilder('card')
-        .where('card.taboo_set_id is null OR card.taboo_set_id = 0')
-        .getCount();
-      return {
-        cardCount,
-        lastModified,
-      };
+    const lastModified = response.headers.get('Last-Modified') || undefined;
+    updateProgress(0.1);
+
+    const json = await response.json();
+    updateProgress(0.2);
+    VERBOSE && console.log('Parsed ArkhamDB json');
+
+    const encounterSets = await db.encounterSets();
+    const cards = await db.cards();
+    const tabooSets = await db.tabooSets();
+    const rules = await db.rules();
+
+    // Delete the tables.
+    await cards.createQueryBuilder().delete().execute();
+    await encounterSets.createQueryBuilder().delete().execute();
+    await tabooSets.createQueryBuilder().delete().execute();
+    await rules.createQueryBuilder().delete().execute();
+    await db.clearCache();
+    VERBOSE && console.log('Cleared old database');
+    updateProgress(0.22);
+
+    await syncRules(db, lang);
+    updateProgress(0.25);
+
+    // console.log(`${await cards.count() } cards after delete`);
+    const genericInvestigator = Card.fromJson({
+      pack_code: 'custom',
+      pack_name: t`Custom`,
+      type_code: 'investigator',
+      type_name: t`Investigator`,
+      faction_code: 'neutral',
+      faction_name: t`Neutral`,
+      position: 1,
+      code: 'custom_001',
+      name: 'Johnny Anybody',
+      real_name: 'Johnny Anybody',
+      subname: t`The Chameleon`,
+      text: t`This is a custom investigator to allow for building of arbitrary decks.\n[elder_sign]: +X where X is whatever you want it to be.`,
+      quantity: 1,
+      skill_willpower: 3,
+      skill_intellect: 3,
+      skill_combat: 3,
+      skill_agility: 3,
+      health: 7,
+      health_per_investigator: false,
+      sanity: 7,
+      deck_limit: 1,
+      deck_requirements: {
+        size: 30,
+        card: {},
+        random: [{ target: 'subtype', value: 'basicweakness' }],
+      },
+      deck_options: [{
+        faction: ['guardian','seeker','rogue','mystic','survivor','neutral'],
+        level: { min: 0, max: 5 },
+      }],
+      is_unique: true,
+      double_sided: true,
+      back_text: '<b>Deck Size</b>: 30.\n<b>Deckbuilding Options</b>: Guardian cards ([guardian]) level 0-5, Seeker cards ([seeker]) level 0-5, Rogue cards ([rogue]) level 0-5, Mystic cards ([mystic]) level 0-5, Survivor cards ([survivor]) level 0-5, Neutral cards level 0-5.\n<b>Deckbuilding Requirements</b> (do not count toward deck size): 1 random basic weakness.',
+    }, packsByCode, cycleNames, lang || 'en');
+    genericInvestigator.browse_visible = 4;
+    const cardsToInsert: Card[] = [genericInvestigator];
+    const dupes: {
+      [code: string]: string[] | undefined;
+    } = {};
+    forEach(json, cardJson => {
+      try {
+        const card = Card.fromJson(cardJson, packsByCode, cycleNames, lang || 'en');
+        if (card) {
+          /*
+          // Code to spit out investigator deck_options localization strings.
+          if (card.type_code === 'investigator' && card.deck_options) {
+            forEach(card.deck_options, option => {
+              if (option.error) {
+                console.log(`'${option.error}': t\`${option.error}\`,`);
+              }
+            });
+          }
+          */
+          if (card.duplicate_of_code) {
+            if (!dupes[card.duplicate_of_code]) {
+              dupes[card.duplicate_of_code] = [];
+            }
+            dupes[card.duplicate_of_code]?.push(card.pack_code);
+          }
+          cardsToInsert.push(card);
+        }
+      } catch (e) {
+        Alert.alert(`${e}`);
+        console.log(e);
+        console.log(cardJson);
+      }
+    });
+    updateProgress(0.35);
+    const linkedSet = new Set(flatMap(cardsToInsert, (c: Card) => c.linked_card ? [c.code, c.linked_card] : []));
+    const dedupedCards = filter(cardsToInsert, (c: Card) => !!c.linked_card || !linkedSet.has(c.code));
+    const flatCards = flatMap(dedupedCards, (c: Card) => {
+      return c.linked_card ? [c, c.linked_card] : [c];
+    });
+    const encounter_card_counts: {
+      [encounter_code: string]: number | undefined;
+    } = {};
+
+    // Clean up all the bonded stuff.
+    const bondedNames: string[] = [];
+    const playerCards: Card[] = [];
+    forEach(flatCards, card => {
+      if (dupes[card.code]) {
+        card.reprint_pack_codes = dupes[card.code];
+      }
+      if (!card.hidden && card.encounter_code) {
+        encounter_card_counts[card.encounter_code] = (encounter_card_counts[card.encounter_code] || 0) + (card.quantity || 1);
+      }
+      if (card.bonded_name) {
+        bondedNames.push(card.bonded_name);
+      }
+      if ((card.deck_limit && card.deck_limit > 0) && !card.spoiler && !(card.xp === undefined || card.xp === null)) {
+        playerCards.push(card);
+      }
+    });
+
+    // Handle all upgrade stuff
+    const cardsByName = values(groupBy(playerCards, card => card.real_name.toLowerCase()));
+    forEach(cardsByName, cardsGroup => {
+      if (cardsGroup.length > 1) {
+        const maxXpCard = head(sortBy(cardsGroup, card => -(card.xp || 0)));
+        if (maxXpCard) {
+          forEach(cardsGroup, card => {
+            const xp = card.xp || 0;
+            card.has_upgrades = xp < (maxXpCard.xp || 0);
+          });
+        }
+      }
+    });
+
+    // Handle all bonded card stuff, and encountercode sizes.
+    const bondedSet = new Set(bondedNames);
+    forEach(flatCards, card => {
+      if (card.encounter_code) {
+        card.encounter_size = encounter_card_counts[card.encounter_code] || 0;
+        encounter_card_counts;
+      }
+      if (bondedSet.has(card.real_name)) {
+        card.bonded_from = true;
+      }
+    });
+
+    // Deal with duplicate ids?
+    forEach(groupBy(flatCards, card => card.id), dupes => {
+      if (dupes.length > 1) {
+        forEach(dupes, (dupe, idx) => {
+          dupe.id = `${dupe.id}_${idx}`;
+        });
+      }
+    });
+    let customCards: Card[] = [];
+    try {
+      const customCardsResponse = await customCardsPromise;
+      customCards = map(customCardsResponse.data.card, customCard => Card.fromGraphQl(customCard, lang || 'en'));
     } catch (e) {
       console.log(e);
-      return Promise.resolve(null);
     }
+    const [linkedCards, normalCards] = partition(dedupedCards, card => !!card.linked_card);
+    const queryRunner = await db.startTransaction();
+    try {
+      VERBOSE && console.log('Parsed all cards');
+      const totalCards = (linkedCards.length + normalCards.length + customCards.length + sumBy(linkedCards, c => c.linked_card ? 1 : 0)) || 3000;
+      let processedCards = 0;
+      async function insertCards(c: Card[]) {
+        await queryRunner.manager.insert(Card, c);
+        processedCards += c.length;
+        updateProgress(0.35 + (processedCards / (1.0 * totalCards) * 0.50));
+      }
+      await insertChunk(flatMap(linkedCards, c => c.linked_card ? [c.linked_card] : []), insertCards);
+      // console.log('Inserted back-link cards');
+      await insertChunk(linkedCards, insertCards);
+      VERBOSE && console.log('Inserted front link cards');
+      await insertChunk(normalCards, insertCards);
+      if (customCards.length) {
+        await insertChunk(customCards, insertCards);
+      }
+    } finally {
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+    }
+
+    VERBOSE && console.log('Inserted normal cards');
+    updateProgress(0.90);
+    const cardCount = await cards.createQueryBuilder('card')
+      .where('card.taboo_set_id is null OR card.taboo_set_id = 0')
+      .getCount();
+    return {
+      cardCount,
+      lastModified,
+    };
   } catch (e) {
     console.log(e);
     throw e;
