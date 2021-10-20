cb19bdbf7db188038093d61cf6949a7b7c10a39b
Daniel Salinas <daniel@occamzrazor.com>
2021-10-20 16:36:46 -0400

    Update weakness placeholder to have weakness icon

--- src/components/card/CardDetailView/PlayerCardImage.tsx
+++ src/components/card/CardDetailView/PlayerCardImage.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 import FastImage from 'react-native-fast-image';
 
+import ArkhamIcon from '@icons/ArkhamIcon';
 import EncounterIcon from '@icons/EncounterIcon';
 import FactionIcon from '@icons/FactionIcon';
 import Card from '@data/types/Card';
@@ -43,6 +44,22 @@ function ImagePlaceholder({ card }: { card: Card }) {
       ]}>
         <Text style={styles.placeholderIcon}>
           <EncounterIcon encounter_code={card.encounter_code} size={55} color="#FFF" />
+        </Text>
+      </View>
+    );
+  }
+  if (card.subtype_code) {
+    return (
+      <View style={[
+        styles.placeholder,
+        { backgroundColor: colors.faction.neutral.background },
+      ]}>
+        <Text style={styles.placeholderIcon}>
+          <ArkhamIcon
+            name="weakness"
+            color="white"
+            size={55}
+          />
         </Text>
       </View>
     );
