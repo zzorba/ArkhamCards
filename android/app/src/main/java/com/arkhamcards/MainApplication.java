1f74b3cd143744a1a7cc148c258063cfd7e54954
Daniel Salinas <daniel@Daniels-MacBook-Pro.local>
2022-07-21 10:44:51 -0400

    Upgrade to RN68.2

--- android/app/src/main/java/com/arkhamcards/MainApplication.java
+++ android/app/src/main/java/com/arkhamcards/MainApplication.java
@@ -9,11 +9,16 @@ import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import android.content.Context;
+import com.facebook.react.config.ReactFeatureFlags;
+import com.arkhamcards.newarchitecture.MainApplicationReactNativeHost;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
+    private final ReactNativeHost mNewArchitectureNativeHost =
+        new MainApplicationReactNativeHost(this);
+
   private final ReactNativeHost mReactNativeHost =
     new NavigationReactNativeHost(this) {
         @Override
@@ -41,12 +46,19 @@ public class MainApplication extends NavigationApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        // If you opted-in for the New Architecture, we enable the TurboModule system
+        ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
         // registerExternalComponent("RNNCustomComponent", new FragmentCreator());
     }
 
     @Override
     public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            return mNewArchitectureNativeHost;
+        } else {
+            return mReactNativeHost;
+        }
     }
 
     @Override
