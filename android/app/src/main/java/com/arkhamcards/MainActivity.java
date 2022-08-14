a046f5d646ab9f7a059aee9c9b9328dff97a6151
Daniel Salinas <daniel@Daniels-MacBook-Pro.local>
2022-08-14 13:58:17 -0400

    RN69
    Nice

--- android/app/src/main/java/com/arkhamcards/MainActivity.java
+++ android/app/src/main/java/com/arkhamcards/MainActivity.java
@@ -38,4 +38,11 @@ public class MainActivity extends NavigationActivity {
         }
         super.addDefaultSplashLayout();
     }
+
+    @Override
+    protected boolean isConcurrentRootEnabled() {
+        // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+        // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+        return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
 }
