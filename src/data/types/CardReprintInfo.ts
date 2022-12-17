6baa326b68e2bef9ecccf88ad34b7748799827f5
Daniel Salinas <arkhamcards@gmail.com>
2022-12-17 08:34:12 -0600

    Add reprint info for cards and quantities

--- src/data/types/CardReprintInfo.ts
+++ src/data/types/CardReprintInfo.ts
@@ -13,11 +13,24 @@ export default class CardReprintInfo {
   @Column('integer', { nullable: true })
   public quantity?: number;
 
+  @Column('integer', { nullable: true })
+  public position?: number;
+
+  @Column('text', { nullable: true })
+  public cycle_code?: string;
+
+  @Column('text', { nullable: true })
+  public cycle_name?: string;
+
   static parse(card: Card): CardReprintInfo {
     const cri = new CardReprintInfo();
     cri.pack_code = card.pack_code;
     cri.pack_name = card.pack_name;
     cri.quantity = card.quantity;
+    cri.position = card.position;
+    cri.cycle_code = card.cycle_code;
+    cri.cycle_name = card.cycle_name;
+
     return cri;
   }
 }
