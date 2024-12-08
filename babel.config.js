5cb1201adc04a39b2a250f76c8d6ab0c8e6fba27
Daniel Salinas <arkhamcards@gmail.com>
2024-12-08 18:28:22 -0500

    RNN is still fucked

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ module.exports = {
   ],
   plugins: [
   	["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ['@babel/plugin-transform-flow-strip-types'],
     ["@babel/plugin-proposal-class-properties", { "loose": true }],
     [
       'module-resolver',
