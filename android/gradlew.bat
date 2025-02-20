f5dc6b2a63ef0591a393f11eaff2ce88d55e1d54
Daniel Salinas <arkhamcards@gmail.com>
2025-02-20 18:47:11 -0500

    Move to RN76

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,3 +1,9 @@
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+@rem SPDX-License-Identifier: Apache-2.0
+@rem
+
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
