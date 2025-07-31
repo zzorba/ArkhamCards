04331f9f4c2c9f615788630ccb280ac2419dc381
Addison Stumpf <addisonstumpf@gmail.com>
2025-07-30 20:40:34 -0500

    Adding card set icons

---
+++ src/icons/CircusExMortisIcon.ts
@@ -0,0 +1,5 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+
+import circusExMortisIconConfig from '../../assets/circusexmortis.json';
+
+export default createIconSetFromIcoMoon(circusExMortisIconConfig);
