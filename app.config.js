159c7b4ff0eecaf85d81d18fe0b6153203482dba
Daniel Salinas <arkhamcards@gmail.com>
2025-12-24 22:24:53 -0600

    Version bump

--- app.config.js
+++ app.config.js
@@ -99,7 +99,7 @@ module.exports = {
         'android.permission.RECORD_AUDIO',
         'android.permission.MODIFY_AUDIO_SETTINGS',
       ],
-      versionCode: 4195556,
+      versionCode: 4195557,
     },
     locales: {
       "ko": "./assets/i18n-app/ko.metadata.json"
